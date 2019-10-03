b3191fb2315abe656651964c534db8755f4d7ab5
bruce <brucetasy@gmail.com>
2019-10-03 10:27:10 +0800

    非同步作業

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -9,21 +9,25 @@ namespace ImageResizer
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images");
-            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); ;
+            string sourcePath = Path.Combine(Environment.CurrentDirectory, "images"); // 圖片來源
+            string destinationPath = Path.Combine(Environment.CurrentDirectory, "output"); //  圖片複製位置
 
-            ImageProcess imageProcess = new ImageProcess();
-
+            ImageProcess imageProcess = new ImageProcess();  
             imageProcess.Clean(destinationPath);
 
-            Stopwatch sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch();  // 
             sw.Start();
-            imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+
+            await imageProcess.ResizeImages(sourcePath, destinationPath, 2.0);
+
             sw.Stop();
 
             Console.WriteLine($"花費時間: {sw.ElapsedMilliseconds} ms");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
         }
     }
 }
+
